cb9d1f8fbbf70f642014c15cd614177903ccf099
vikaskelageri <vikaskelageri5@gmail.com>
2025-04-20 17:08:46 +0530

    updated

--- src/test/java/test/AgentAPI.java
+++ src/test/java/test/AgentAPI.java
@@ -35,51 +35,51 @@ public class AgentAPI extends BaseTest {
 
     }
 
-    @Test(priority = 2, groups = "Create")
-    public void testCreateAgent() {
-        String payload = "";
-        try {
-            payload = new String(Files.readAllBytes(Paths.get("src/test/resourses/payloads/AgentPayload.json")));
-        } catch (IOException e) {
-            System.err.println("Error reading AgentPayload.json: " + e.getMessage());
-            org.testng.Assert.fail("Failed to read AgentPayload.json file.");
-            return;
-        }
-
-        Response response = given()
-                .header("Content-Type", "application/json")
-                .body(payload)
-                .when()
-                .post("/api/v1/user/signup")
-                .then()
-                .statusCode(201)
-                .extract().response();
-
-        Reporter.getCurrentTestResult().setAttribute("response", response);
-
-        System.out.println("Successful: " + response.asString());
-        agentId = response.jsonPath().getString("id");
-    }
-
-    @Test(priority = 3, groups = "Update")
-    public void CPI_UpdateAgent_() {
-
-        String payload = "";
-        try {
-            payload = new String(Files.readAllBytes(Paths.get("src/test/resourses/payloads/Agent_update.json")));
-        } catch (IOException e) {
-            System.err.println("Error reading AgentPayload.json: " + e.getMessage());
-            org.testng.Assert.fail("Failed to read AgentPayload.json file.");
-            return;
-        }
-
-        Response res=given()
-                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ3MzIxMTg5LCJpYXQiOjE3NDQ3MjkxODksImp0aSI6IjRmZGNkNTFjZGI3MTQ2YzVhZDhjNTUzMDYwZWVkODA3IiwidXNlcl9pZCI6ODl9.6cDhidmoHm1ZO77KbAtCWIc6HNYRgoAdIGToCD28y1o")
-                .body(payload)
-                .post("/api/v1/user/111/manage")
-                .then().extract().response();
-        System.out.println(res.asString());
-
-
-    }
+//    @Test(priority = 2, groups = "Create")
+//    public void testCreateAgent() {
+//        String payload = "";
+//        try {
+//            payload = new String(Files.readAllBytes(Paths.get("src/test/resourses/payloads/AgentPayload.json")));
+//        } catch (IOException e) {
+//            System.err.println("Error reading AgentPayload.json: " + e.getMessage());
+//            org.testng.Assert.fail("Failed to read AgentPayload.json file.");
+//            return;
+//        }
+//
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(payload)
+//                .when()
+//                .post("/api/v1/user/signup")
+//                .then()
+//                .statusCode(201)
+//                .extract().response();
+//
+//        Reporter.getCurrentTestResult().setAttribute("response", response);
+//
+//        System.out.println("Successful: " + response.asString());
+//        agentId = response.jsonPath().getString("id");
+//    }
+//
+//    @Test(priority = 3, groups = "Update")
+//    public void CPI_UpdateAgent_() {
+//
+//        String payload = "";
+//        try {
+//            payload = new String(Files.readAllBytes(Paths.get("src/test/resourses/payloads/Agent_update.json")));
+//        } catch (IOException e) {
+//            System.err.println("Error reading AgentPayload.json: " + e.getMessage());
+//            org.testng.Assert.fail("Failed to read AgentPayload.json file.");
+//            return;
+//        }
+//
+//        Response res=given()
+//                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ3MzIxMTg5LCJpYXQiOjE3NDQ3MjkxODksImp0aSI6IjRmZGNkNTFjZGI3MTQ2YzVhZDhjNTUzMDYwZWVkODA3IiwidXNlcl9pZCI6ODl9.6cDhidmoHm1ZO77KbAtCWIc6HNYRgoAdIGToCD28y1o")
+//                .body(payload)
+//                .post("/api/v1/user/111/manage")
+//                .then().extract().response();
+//        System.out.println(res.asString());
+//
+//
+//    }
 }
